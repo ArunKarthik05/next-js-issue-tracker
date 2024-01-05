9be7e73e27841c74524b20b98fe432a4ce00fa62
karthikak05 <akkarthikk2002@gmail.com>
2024-01-05 20:39:23 +0530

    inital

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,11 +1,15 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { IoBug } from "react-icons/io5";
+import classnames from "classnames";
 
 export default function NavBar() {
+  const currentPath = usePathname();
   const links = [
-    { label: "Deashboard", href: "/" },
-    { label: "Issues", href: "/" },
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
   ];
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
@@ -17,7 +21,11 @@ export default function NavBar() {
           <Link
             href={link.href}
             key={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classnames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover-text-zinc-800 transition-colors": true,
+            })}
           >
             {link.label}
           </Link>
